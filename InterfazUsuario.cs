f463fbfbaeaff14c362d66f1b491bf57a6eef293
Victoria <vicky.bracho.123@gmail.com>
2024-10-20 18:24:30 -0600

    Agregar archivos de proyecto.

---
+++ InterfazUsuario.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace POO_PSAM_P10
+{
+    public class InterfazUsuario
+    {
+        private List<Opcion> opcionesPrincipal;
+        private Menu menuPrincipal;
+        private bool dentroMenuPrincipal = true;
+
+        public InterfazUsuario()
+        {
+            opcionesPrincipal = new List<Opcion>()
+        {
+            new Opcion("Carrera", Carrera),
+            new Opcion("Materia", Materia),
+            new Opcion("Grupo", Grupo),
+            new Opcion("Maestro", Maestro),
+            new Opcion("Alumno", Alumno),
+            new Opcion("Salir", Salir),
+        };
+
+            menuPrincipal = new Menu("Menú Escuela", opcionesPrincipal);
+
+            while (dentroMenuPrincipal)
+            {
+                menuPrincipal.MostrarMenu();
+                int opcion = menuPrincipal.SeleccionarOpcion();
+                opcionesPrincipal[opcion - 1].Action.Invoke();
+            }
+        }
+
+        public void Carrera()
+        {
+            bool dentroMenuCarrera = true;
+
+            List<Opcion> opcionesCarrera = new List<Opcion>
+        {
+            new Opcion("Registrar carrera.", RegistrarCarrera),
+            new Opcion("Ver carreras existentes.", () => Console.WriteLine(Escuela.MostrarCarreras())),
+            new Opcion("Actualizar carrera existente", ActualizarCarrera),
+            new Opcion("Eliminar carrera existente.", () => {
+                Console.Write("Ingrese el ID de la carrera que desea eliminar: ");
+                Console.WriteLine(Escuela.EliminarCarrera(int.Parse(Console.ReadLine())));
+            }),
+            new Opcion("Regresar a Menú Principal", () => { dentroMenuCarrera = false; RegresarPrincipal(); })
+        };
+
+            Menu menuCarrera = new Menu("Menú de las Carreras", opcionesCarrera);
+
+            while (dentroMenuCarrera)
+            {
+                menuCarrera.MostrarMenu();
+                int opcion = menuCarrera.SeleccionarOpcion();
+                Console.WriteLine();
+                opcionesCarrera[opcion - 1].Action.Invoke();
+                menuCarrera.Continuar();
+            }
+        }
+
+        public void RegistrarCarrera()
+        {
+            Console.Write("Ingrese el nombre de la carrera: ");
+            string nombreCarrera = Console.ReadLine();
+
+            Console.Write("Ingrese el ID de la carrera: ");
+            int idCarrera = int.Parse(Console.ReadLine());
+
+            Carrera carrera = new Carrera(idCarrera, nombreCarrera);
+            Escuela.AgregarCarrera(carrera);
+
+            Console.WriteLine("Carrera registrada exitosamente.");
+        }
+
+        public void ActualizarCarrera()
+        {
+            Console.Write("Ingrese el ID de la carrera que desea actualizar: ");
+            int idCarrera = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el nuevo nombre de la carrera: ");
+            string nuevoNombre = Console.ReadLine();
+
+            Console.Write("Ingrese el nuevo ID de la carrera: ");
+            int nuevoIdCarrera = int.Parse(Console.ReadLine());
+
+            Escuela.ActualizarCarrera(idCarrera, nuevoNombre, nuevoIdCarrera);
+            Console.WriteLine("Carrera actualizada exitosamente.");
+        }
+
+        public void Materia()
+        {
+            bool dentroMenuMateria = true;
+
+            List<Opcion> opcionesMateria = new List<Opcion>
+        {
+            new Opcion("Registrar materia.", RegistrarMateria),
+            new Opcion("Ver materias existentes.", () => Console.WriteLine(Escuela.MostrarMaterias())),
+            new Opcion("Actualizar materia existente", ActualizarMateria),
+            new Opcion("Eliminar materia existente.", () => {
+                Console.Write("Ingrese el ID de la materia que desea eliminar: ");
+                Console.WriteLine(Escuela.EliminarMateria(int.Parse(Console.ReadLine())));
+            }),
+            new Opcion("Regresar a Menú Principal", () => { dentroMenuMateria = false; RegresarPrincipal(); })
+        };
+
+            Menu menuMateria = new Menu("Menú de las Materias", opcionesMateria);
+
+            while (dentroMenuMateria)
+            {
+                menuMateria.MostrarMenu();
+                int opcion = menuMateria.SeleccionarOpcion();
+                Console.WriteLine();
+                opcionesMateria[opcion - 1].Action.Invoke();
+                menuMateria.Continuar();
+            }
+        }
+
+        public void RegistrarMateria()
+        {
+            Console.Write("Ingrese el nombre de la materia: ");
+            string nombreMateria = Console.ReadLine();
+
+            Console.Write("Ingrese el ID de la materia: ");
+            int idMateria = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el número de créditos de la materia: ");
+            int numeroCreditos = int.Parse(Console.ReadLine());
+
+            Materia materia = new Materia(idMateria, nombreMateria, numeroCreditos);
+            Escuela.AgregarMateria(materia);
+
+            Console.WriteLine("Materia registrada exitosamente.");
+        }
+
+        public void ActualizarMateria()
+        {
+            Console.Write("Ingrese el ID de la materia que desea actualizar: ");
+            int idMateria = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el nuevo nombre de la materia: ");
+            string nuevoNombre = Console.ReadLine();
+
+            Console.Write("Ingrese el nuevo número de créditos de la materia: ");
+            int nuevosCreditos = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el nuevo ID de la materia: ");
+            int nuevoIdMateria = int.Parse(Console.ReadLine());
+
+            Escuela.ActualizarMateria(idMateria, nuevoNombre, nuevosCreditos, nuevoIdMateria);
+            Console.WriteLine("Materia actualizada exitosamente.");
+        }
+
+        public void Grupo()
+        {
+            bool dentroMenuGrupo = true;
+
+            List<Opcion> opcionesGrupo = new List<Opcion>
+        {
+            new Opcion("Crear grupo.", RegistrarGrupo),
+            new Opcion("Ver grupos existentes.", () => Console.WriteLine(Escuela.MostrarGrupos())),
+            new Opcion("Actualizar grupo existente", ActualizarGrupo),
+            new Opcion("Eliminar grupo existente.", () => {
+                Console.Write("Ingrese el ID del grupo que desea eliminar: ");
+                Console.WriteLine(Escuela.EliminarGrupo(int.Parse(Console.ReadLine())));
+            }),
+            new Opcion("Regresar a Menú Principal", () => { dentroMenuGrupo = false; RegresarPrincipal(); })
+        };
+
+            Menu menuGrupo = new Menu("Menú del Grupo", opcionesGrupo);
+
+            while (dentroMenuGrupo)
+            {
+                menuGrupo.MostrarMenu();
+                int opcion = menuGrupo.SeleccionarOpcion();
+                Console.WriteLine();
+                opcionesGrupo[opcion - 1].Action.Invoke();
+                menuGrupo.Continuar();
+            }
+        }
+
+        public void RegistrarGrupo()
+        {
+            Console.Write("Ingrese el ID del grupo: ");
+            int idGrupo = int.Parse(Console.ReadLine());
+
+            Grupo grupo = new Grupo(idGrupo);
+            Escuela.AgregarGrupo(grupo);
+
+            Console.WriteLine("Grupo registrado exitosamente.");
+        }
+
+        public void ActualizarGrupo()
+        {
+            Console.Write("Ingrese el ID del grupo que desea actualizar: ");
+            int idGrupo = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el nuevo nombre del grupo: ");
+            string nuevoNombre = Console.ReadLine();
+
+            Console.Write("Ingrese el nuevo ID del grupo: ");
+            int nuevoIdGrupo = int.Parse(Console.ReadLine());
+
+            Escuela.ActualizarGrupo(idGrupo, nuevoNombre, nuevoIdGrupo);
+            Console.WriteLine("Grupo actualizado exitosamente.");
+        }
+
+        public void Maestro()
+        {
+            bool dentroMenuMaestro = true;
+
+            List<Opcion> opcionesMaestro = new List<Opcion>
+        {
+            new Opcion("Registrar maestro.", RegistrarMaestro),
+            new Opcion("Ver maestros existentes.", () => Console.WriteLine(Escuela.MostrarMaestros())),
+            new Opcion("Asignar maestro a grupo", () => {
+                Console.Write("Ingrese el ID del maestro: ");
+                int idMaestro = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el ID del grupo: ");
+                int idGrupo = int.Parse(Console.ReadLine());
+                Console.WriteLine(Escuela.AsignarMaestroAGrupo(idMaestro, idGrupo));
+            }),
+            new Opcion("Actualizar maestro existente", ActualizarMaestro),
+            new Opcion("Eliminar maestro existente.", () => {
+                Console.Write("Ingrese el ID del maestro que desea eliminar: ");
+                Console.WriteLine(Escuela.EliminarMaestro(int.Parse(Console.ReadLine())));
+            }),
+            new Opcion("Regresar a Menú Principal", () => { dentroMenuMaestro = false; RegresarPrincipal(); })
+        };
+
+            Menu menuMaestro = new Menu("Menú del Maestro", opcionesMaestro);
+
+            while (dentroMenuMaestro)
+            {
+                menuMaestro.MostrarMenu();
+                int opcion = menuMaestro.SeleccionarOpcion();
+                Console.WriteLine();
+                opcionesMaestro[opcion - 1].Action.Invoke();
+                menuMaestro.Continuar();
+            }
+        }
+
+        public void RegistrarMaestro()
+        {
+            Console.Write("Ingrese el ID del maestro: ");
+            int idMaestro = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el nombre del maestro: ");
+            string nombreMaestro = Console.ReadLine();
+
+            Console.Write("Ingrese el correo del maestro: ");
+            string correoMaestro = Console.ReadLine();
+
+            Maestro maestro = new Maestro(idMaestro, nombreMaestro, correoMaestro);
+            Escuela.AgregarMaestro(maestro);
+
+            Console.WriteLine("Maestro registrado exitosamente.");
+        }
+
+        public void ActualizarMaestro()
+        {
+            Console.Write("Ingrese el ID del maestro que desea actualizar: ");
+            int idMaestro = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el nuevo nombre del maestro: ");
+            string nuevoNombre = Console.ReadLine();
+
+            Console.Write("Ingrese el nuevo correo del maestro: ");
+            string nuevoCorreo = Console.ReadLine();
+
+            Console.Write("Ingrese el nuevo ID del maestro: ");
+            int nuevoIdMaestro = int.Parse(Console.ReadLine());
+
+            Escuela.ActualizarMaestro(idMaestro, nuevoNombre, nuevoCorreo, nuevoIdMaestro);
+            Console.WriteLine("Maestro actualizado exitosamente.");
+        }
+
+        public void Alumno()
+        {
+            bool dentroMenuAlumno = true;
+
+            List<Opcion> opcionesAlumno = new List<Opcion>
+        {
+            new Opcion("Registrar alumno.", RegistrarAlumno),
+            new Opcion("Ver alumnos existentes.", () => Console.WriteLine(Escuela.MostrarAlumnos())),
+            new Opcion("Asignar alumno a grupo", () => {
+                Console.Write("Ingrese el ID del alumno: ");
+                int idAlumno = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el ID del grupo: ");
+                int idGrupo = int.Parse(Console.ReadLine());
+                Console.WriteLine(Escuela.AsignarAlumnoAGrupo(idAlumno, idGrupo));
+            }),
+            new Opcion("Actualizar alumno existente", ActualizarAlumno),
+            new Opcion("Eliminar alumno existente.", () => {
+                Console.Write("Ingrese el ID del alumno que desea eliminar: ");
+                Console.WriteLine(Escuela.EliminarAlumno(int.Parse(Console.ReadLine())));
+            }),
+            new Opcion("Regresar a Menú Principal", () => { dentroMenuAlumno = false; RegresarPrincipal(); })
+        };
+
+            Menu menuAlumno = new Menu("Menú del Alumno", opcionesAlumno);
+
+            while (dentroMenuAlumno)
+            {
+                menuAlumno.MostrarMenu();
+                int opcion = menuAlumno.SeleccionarOpcion();
+                Console.WriteLine();
+                opcionesAlumno[opcion - 1].Action.Invoke();
+                menuAlumno.Continuar();
+            }
+        }
+
+        public void RegistrarAlumno()
+        {
+            Console.Write("Ingrese el ID del alumno: ");
+            int idAlumno = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el nombre del alumno: ");
+            string nombreAlumno = Console.ReadLine();
+
+            Alumno alumno = new Alumno(idAlumno, nombreAlumno);
+            Escuela.AgregarAlumno(alumno);
+
+            Console.WriteLine("Alumno registrado exitosamente.");
+        }
+
+        public void ActualizarAlumno()
+        {
+            Console.Write("Ingrese el ID del alumno que desea actualizar: ");
+            int idAlumno = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el nuevo nombre del alumno: ");
+            string nuevoNombre = Console.ReadLine();
+
+            Console.Write("Ingrese el nuevo ID del alumno: ");
+            int nuevoIdAlumno = int.Parse(Console.ReadLine());
+
+            Escuela.ActualizarAlumno(idAlumno, nuevoNombre, nuevoIdAlumno);
+            Console.WriteLine("Alumno actualizado exitosamente.");
+        }
+
+        public void Salir()
+        {
+            Console.WriteLine("Saliendo del programa...");
+            dentroMenuPrincipal = false;
+        }
+
+        public void RegresarPrincipal()
+        {
+            dentroMenuPrincipal = true;
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This.team
+{
+    internal class InterfazUsuario
+    {
+        private Escuela escuela;
+        //Lista de opciones que se muestran en el menú.
+        private List<Opcion> opciones;
+
+        //Constructor de la clase InterfazUsuario.
+        public InterfazUsuario()
+        {
+            //Inicializar la instancia de la escuela.
+            escuela = new Escuela();
+            //Inicializar la lista de opciones para el menú.
+            opciones = new List<Opcion>();
+            {
+                opciones.Add(new Opcion("Crear grupo.", CrearGrupo));
+                opciones.Add(new Opcion("Registrar profesor.", RegistrarProfesor));
+                opciones.Add(new Opcion("Registrar alumno.", RegistrarAlumno));
+                opciones.Add(new Opcion("Inscribir alumno a un grupo.", AsignarAlumnoAGrupo));
+                opciones.Add(new Opcion("Asignar profesor a un grupo.", AsignarProfesorAGrupo));
+                opciones.Add(new Opcion("Ver grupos existentes.", MostrarGrupos));
+                opciones.Add(new Opcion("Ver alumnos existentes.", MostrarAlumnos));
+                opciones.Add(new Opcion("Ver profesores existentes.", MostrarProfesores));
+                opciones.Add(new Opcion("Ver carreras existentes.", MostrarCarreras));
+                opciones.Add(new Opcion("Ver alumnos inscritos en un curso.", MostrarAlumnosEnCurso));
+                opciones.Add(new Opcion("Salir", () => Environment.Exit(0)));
+            }
+        }
+
+        //Método principal que inicia la interacción con el usuario.
+        public void Iniciar()
+        {
+            while (true)
+            {
+                //Mostrar el menú de opciones.
+                MostarMenu();
+                //Ejecutar la opción seleccionada por el usuario.
+                SeleccionarOpcion();
+            }
+        }
+
+        //Método que muestra las opciones del menú en la consola.
+        private void MostarMenu()
+        {
+            Console.WriteLine("Menú de la escuela:\n");
+            //Recorrer la lista de opciones.
+            for (int i = 0; i < opciones.Count; i++)
+            {
+                //Mostrar cada opción con su número correspondiente.
+                Console.WriteLine($"{i + 1}. {opciones[i].Message}");
+            }
+        }
+
+        //Método que solicita al usuario seleccionar una opción del menú y ejecuta la acción correspondiente.
+        private void SeleccionarOpcion()
+        {
+            Console.Write("\nSelecciona una opción: ");
+            try
+            {
+                //Leer y ajustar la opción seleccionada.
+                int numOpcion = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                if (numOpcion >= 0 && numOpcion < opciones.Count)
+                {
+                    //Ejecutar la acción de la opción seleccionada.
+                    Console.Clear();
+                    opciones[numOpcion].Action.Invoke();
+                }
+                else
+                {
+                    //Vuelve a solicitar la selección de opción si la opción no es válida.
+                    Console.WriteLine("Opción no válida. Inténtalo nuevamente.\n");
+                    MostarMenu();
+                    SeleccionarOpcion();
+                }
+            }
+            catch (Exception e)
+            {
+                //Volver a mostrar el menú y la solicitud de selección de opción en caso de error.
+                Console.WriteLine("No has introducido un valor válido.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                MostarMenu();
+                SeleccionarOpcion();
+            }
+        }
+
+        //Método para crear un nuevo curso.
+        public void CrearCurso()
+        {
+            //Solicita el nombre del curso al usuario.
+            Console.WriteLine("Ingresar el nombre del curso: ");
+            string nombreCurso = Console.ReadLine();
+            Console.WriteLine();
+
+            escuela.CrearCurso(nombreCurso);
+
+            //Mostrar un mensaje confirmando que el curso fue registrado exitosamente.
+            Console.WriteLine($"Curso creado exitosamente.\n");
+            Continuar();
+        }
+
+        //Método para asignar un nuevo profesor en la escuela.
+        public void RegistrarProfesor()
+        {
+            {
+                //Solicita el nombre del profesor al usuario.
+                Console.WriteLine("Ingresar el nombre del profesor: ");
+                string nombreProfesor = Console.ReadLine();
+                Console.WriteLine();
+
+                //Solicita la especialidad del profesor al usuario.
+                Console.WriteLine("Ingresar la especialidad del profesor: ");
+                string especialidad = Console.ReadLine();
+                Console.WriteLine();
+
+                //Llamar al método de la escuela para asignar al profesor.
+                escuela.RegistrarProfesor(nombreProfesor, especialidad);
+
+                //Mostrar un mensaje confirmando que el profesor fue registrado exitosamente.
+                Console.WriteLine($"Profesor registrado exitosamente.\n");
+                Continuar();
+            }
+        }
+
+        //Método para inscribir a un nuevo alumno en la escuela.
+        public void RegistrarAlumno()
+        {
+            //Solicita el ID del alumno al usuario.
+            Console.WriteLine("Ingresar el ID del alumno: ");
+            int idAlumno = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //Solicita el nombre del alumnoo al usuario.
+            Console.WriteLine("Ingresar el nombre del alumno: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine();
+
+            // Solicita la edad del alumno al usuario.
+            Console.WriteLine("Ingresar la edad del alumno: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //Llamar al método de la escuela para inscribir al alumno.
+            escuela.InscribirAlumno(idAlumno, nombre, edad);
+
+            //Mostrar un mensaje confirmando que el alumno fue inscrito exitosamente.
+            Console.WriteLine($"Alumno inscrito exitosamente.\n");
+            Continuar();
+
+        }
+
+        //Método para asignar un alumno a un curso.
+        public void AsignarAlumnoACurso()
+        {
+            //Solicita el ID del alumno al usuario.
+            Console.WriteLine("Ingresar el ID del alumno: ");
+            int idAlumno = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //Solicita el nombre del curso al usuario.
+            Console.WriteLine("Ingresar el nombre del curso: ");
+            string nombreCurso = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine(escuela.AsignarAlumnoACurso(idAlumno, nombreCurso));
+            Continuar();
+        }
+
+        //Método para asignar un profesor a un curso.
+        public void AsignarProfesorACurso()
+        {
+            //Solicita el nombre del profesor al usuario.
+            Console.WriteLine("Ingresar el nombre del profesor: ");
+            string nombreProfesor = Console.ReadLine();
+            Console.WriteLine();
+
+            //Solicita el nombre del curso al usuario.
+            Console.WriteLine("Ingresar el nombre del curso: ");
+            string nombreCurso = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine(escuela.AsignarProfesorACurso(nombreProfesor, nombreCurso));
+            Continuar();
+
+
+        }
+
+        //Método para mostrar los cursos existentes en la escuela.
+        public void MostrarCursos()
+        {
+            Console.WriteLine(escuela.VerCursos());
+            Continuar();
+        }
+
+        //Método para mostrar los alumnos inscritos en un curso.
+        public void MostrarAlumnosEnCurso()
+        {
+            //Solicita el nombre del curso al usuario.
+            Console.WriteLine("Ingresar el nombre del curso: ");
+            string nombreCurso = Console.ReadLine();
+            Console.WriteLine();
+
+            //Llamar al método de la escuela para mostrar los alumnos inscritos en el curso.
+            Console.WriteLine(escuela.VerAlumnos(nombreCurso));
+            Continuar();
+
+        }
+
+        public void Continuar() // Pausa la ejecución y espera a que el usuario presione una tecla para continuar.
+        {
+            Console.WriteLine("\nPresione cualquier tecla para continuar...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
